cb3d8ce8f6632419af1ce7156e28c57fc1420534
Daniyar <shergazievdan@gmail.com>
2022-03-07 12:50:38 +0600

    redux_without_react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+const initailState = {value: 0};
+
+const reducer = (state = initailState, action) => {
+
+    switch  (action.type) {
+        case 'INC':
+            return {
+                ...state,
+                value: state.value + 1
+            };
+        case 'DEC':
+            return {
+                ...state,
+                value: state.value - 1
+            };
+        case 'RND':
+            return {
+                ...state,
+                value: state.value * action.payload
+            }; ;
+        default: 
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+    document.getElementById('counter').textContent = store.getState().value;
+}
+
+store.subscribe(update);
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', payload: value});
+
+
+document.getElementById('inc').addEventListener('click', () => {
+    store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+    store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    store.dispatch(rnd(value));
+});
+
+
+
+
+
+
+
+
+// let state = reducer(initailState, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+// state = reducer(state, {type: 'INC'});
+
+// console.log(state);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+    
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
